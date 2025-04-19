81ef7105255e81c0355a68f45afa838d0f469cda
Kanisorn Sangchai <karamahati@gmail.com>
2025-04-19 23:17:53 +0700

    chore: clean up and add comments

--- classes/URARM.py
+++ classes/URARM.py
@@ -5,27 +5,20 @@ import math
 
 class URARM:
 
+    # Home position for gripper and joint angles
     HOME_X = 0.116
     HOME_Y = -0.300
     HOME_Z = 0.08
     HOME_RX = 2.233
     HOME_RY = 2.257
     HOME_RZ = -0.039 
-    # HOME_JOINT = [-0.8062151114093226, -1.6997678915606897, -1.0278661886798304, -2.017801109944479, 1.5453407764434814, 2.3251354694366455]
     HOME_JOINT = [1.6437987089157104, -1.2341769377337855, 0.41770029067993164, -0.7451713720904749, -1.6114829222308558, 1.6332783699035645]
     ROTATE_TCP_SLEEP = 1
     MOVEL_SLEEP = 1.5
     TOTAL_SLEEP = ROTATE_TCP_SLEEP + (2 * MOVEL_SLEEP)
-    # HOME_X = 0.116
-    # HOME_Y = -0.300
-    # HOME_Z = 0.2
-    # HOME_RX = 126 * math.pi / 180
-    # HOME_RY = 128 * math.pi / 180
-    # HOME_RZ = 0
-    # HOME_JOINT = [1.6437866687774658, -1.2342007795916956, 0.4176163673400879, -0.7450755278216761, -1.6113866011248987, 1.6333143711090088]
 
     def __init__(self, robot_ip: str, robot_port: int = 30003):
-        print(self.TOTAL_SLEEP)
+        # Initialize the socket connection to the robot
         self.arm = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.arm.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.arm.connect((robot_ip, robot_port))
@@ -36,30 +29,31 @@ class URARM:
             print('Connected to Robot RTDE...FAILED!')
 
     def move_home(self):
+        # Move the robot to the home position
         print('Robot start moving to home position')
         cmd_move = str.encode(f'movel(p[{self.HOME_X},{self.HOME_Y},{self.HOME_Z},{self.HOME_RX},{self.HOME_RY},{self.HOME_RZ}],a=0.5,v=0.5,t=1,r=0)\n')
         self.arm.send(cmd_move)
         time.sleep(5)
 
     def movel(self, pose, a: float = 0.5, v: float = 0.5, t: float = 0, r: float = 0):
+        # Move the gripper to specified pose linearly
         self.arm.send(f"movel({pose},{a},{v},{t},{r})\n".encode("UTF-8"))
-        # time.sleep(self.MOVEL_SLEEP)
         if 0.75*t > 0:
             time.sleep(0.75*t)
         else:
             time.sleep(self.MOVEL_SLEEP)
 
     def movej(self, pose, a: float = 0.5, v: float = 0.5, t: float = 0, r: float = 0):
+        # Move the gripper to specified pose using movej
         self.arm.send(f"movej({pose},{a},{v},{t},{r})\n".encode("UTF-8"))
         if 0.75*t > 0:
             time.sleep(0.75*t)
         else:
             time.sleep(self.MOVEL_SLEEP)
-        # time.sleep(1.5)
     
     def rotate_TCP(self, rx: float = 0, ry: float = 0, rz: float = 0, t: float = 0):
+        # Rotate the gripper
         self.movel(f'[{self.HOME_JOINT[0]},{self.HOME_JOINT[1]},{self.HOME_JOINT[2]},{self.HOME_JOINT[3] + rx},{self.HOME_JOINT[4] + ry},{self.HOME_JOINT[5] + rz}]', t=t)
-        # time.sleep(self.ROTATE_TCP_SLEEP)
         if 0.75*t > 0:
             time.sleep(0.75*t)
         else:
@@ -80,8 +74,10 @@ class URARM:
         return tcp_pose
 
     def grab_after_t(self, x_rel: float, y_rel: float, rz: float, t1: float, t2: float, t3: float, t4: float):
+        # Calculate the total time it would take to grab the box
         total_time = t1 + t2 + t3 + t4
         print("Total time:", total_time)
+        # Calculate the meeting point based on the x position and total time
         x_m = x_rel - (0.02 * (total_time - 2.5))
         print("Meeting point:", x_m)
         self.rotate_TCP(rz=rz, t=t1)
--- main.py
+++ main.py
@@ -7,28 +7,26 @@ import math
 import socket
 
 def main():
-    # Define IP addresses for the robot, vision system, and conveyor belt
+    # Define IP addresses for the robot and the vision system
     robot_ip = '10.10.0.14'
     vs_ip = '10.10.1.10'
-    conv_ip = '0.0.0.0'
 
     # Initialize the robot, gripper, conveyor belt, and vision system
     robot = URARM(robot_ip)
     gripper = Gripper(robot_ip)
     vision = VisionSystem(vs_ip)
 
-
     # Move the robot to the home position
     robot.move_home()
-    # robot.get_current_joint_angle();
 
-    # Initialize the conveyor belt
+    # Initialize the connection with conveyor belt
     c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    c.bind((conv_ip, 2002))
+    c.bind(('0.0.0.0', 2002))
     c.listen(1)
     print("Conveyor belt socket is listening")
     conv, addr = c.accept()
     print(f"Connected by {addr}")
+    # Activate the conveyor belt
     conv.sendall(b'activate,tcp\n')
     time.sleep(1)
     conv.sendall(b'pwr_on,conv,0\n')
@@ -36,11 +34,6 @@ def main():
     conv.sendall(b'set_vel,conv,30\n')
     time.sleep(1)
     conv.sendall(b'jog_fwd,conv,0\n')
-    # time.sleep(3)
-    # conv.sendall(b'jog_stop,conv,0\n')
-    # conveyor = ConveyorBelt(conv_ip)
-    # time.sleep(5)  # Wait for the conveyor to start
-    # conveyor.stop()  # Stop the conveyor belt
 
     # Get and process the x, y coordinates and orientation of the object from the vision system
     x_x1, x_x2, x_ang, xc_x1, xc_x2, xc_ang, y_y1, y_y2, yc_y1, yc_y2 = vision.receive_data()
@@ -50,19 +43,14 @@ def main():
 
     # Move the robot to the object location and pick up the object
     robot.grab_after_t(x_rel=x_coor_rel, y_rel=y_coor_rel, rz=-degree, t1=0.7, t2=0.7, t3=0.7, t4=0.7)
-    # robot.rotate_TCP(rz=-degree)
-    # robot.movel(URARM.relative_pose(z=-0.17)) # Move down to allow movement to the object (might remove if not needed)
-    # robot.movel(URARM.relative_pose(x=x_coor_rel, y=y_coor_rel))
-    # robot.movel(URARM.relative_pose(z=-0.16))
     gripper.control_gripper(True)
-    # robot.movel(URARM.relative_pose(z=0.16))
     robot.move_home()
 
-    print(conv)
+    # Stop the conveyor belt
     conv.sendall(b'jog_stop,conv,0\n')
     time.sleep(1)
-    print(conv.recv(1024).decode())
 
+# Move the robot to the home position
 def home():
     # Define IP addresses for the robot, vision system, and conveyor belt
     robot_ip = '10.10.0.14'
